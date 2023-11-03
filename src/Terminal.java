c7b940f39b7fd399181e75367000b2afa6b7cf20
youssefahmedzakaria <youssefahmed052@gmail.com>
2023-11-03 15:08:33 +0200

    `removeDriectory` modified

--- src/Terminal.java
+++ src/Terminal.java
@@ -39,9 +39,9 @@ public class Terminal {
     private List<String> commandHistory = new ArrayList<>();
 
     public void chooseCommandAction(String command, String[] args) {
-        if(command.equals("history")){
+        if (command.equals("history")) {
             printCommandHistory();
-        }else{
+        } else {
             switch (command) {
                 case "echo": {
                     if (args.length == 0) {
@@ -52,9 +52,9 @@ public class Terminal {
                     break;
                 }
                 case "pwd": {
-                    if(args.length!=0) {
+                    if (args.length != 0) {
                         System.out.println("You should not provide any arguments with pwd command!");
-                    }else {
+                    } else {
                         pwdCommand(args);
                     }
                     break;
@@ -62,12 +62,12 @@ public class Terminal {
                 case "cd":
                     changeDirectoryCommand(args);
                     break;
-                case "ls":{
-                    if(args.length == 0){
+                case "ls": {
+                    if (args.length == 0) {
                         listDirectoryCommand();
-                    }else if(args.length == 1 && Objects.equals(args[0], "-r")){
+                    } else if (args.length == 1 && Objects.equals(args[0], "-r")) {
                         listDirectoryReverse();
-                    }else{
+                    } else {
                         System.out.println("invalid argument!");
                     }
                     break;
@@ -83,40 +83,40 @@ public class Terminal {
                 case "rmdir": {
                     if (args.length == 1) {
                         removeDirectory(args);
-                    }else{
+                    } else {
                         System.out.println("One argument is provided with the 'rmdir' command!");
                     }
                     break;
                 }
-                case "touch":{
-                    if (args.length == 1){
+                case "touch": {
+                    if (args.length == 1) {
                         createFile(args);
-                    }else{
+                    } else {
                         System.out.println("One argument is provided with the 'touch' command!");
                     }
                 }
-                    break;
-                case "ncp":{
-                    if(args.length == 2 && !Objects.equals(args[0], "-r")){
+                break;
+                case "cp": {
+                    if (args.length == 2 && !Objects.equals(args[0], "-r")) {
                         copyFile(args);
-                    }else if(args.length == 3 && Objects.equals(args[0], "-r")){
+                    } else if (args.length == 3 && Objects.equals(args[0], "-r")) {
                         cpDirectory(args);
-                    }else{
+                    } else {
                         System.out.println("invalid argument!");
                     }
                     break;
                 }
                 case "rm":
-                    if(args.length == 1){
+                    if (args.length == 1) {
                         removeFile(args);
-                    }else{
+                    } else {
                         System.out.println("One argument is provided with the 'rm' command");
                     }
                     break;
                 case "wc":
-                    if(args.length == 1){
+                    if (args.length == 1) {
                         wordCount(args);
-                    }else{
+                    } else {
                         System.out.println("One argument is provided with the 'wc' command");
                     }
                     break;
@@ -127,27 +127,31 @@ public class Terminal {
                     System.out.println("Command not recognized.");
             }
         }
-
     }
+
+
     public void printCommandHistory() {
         for (int i = 0; i < commandHistory.size(); i++) {
             System.out.println((i + 1) + " " + commandHistory.get(i));
         }
     }
 
+
     public void echoCommand(String[] args) {
-       for (String arg : args) {
-                System.out.print(arg + " ");
-       }
-       System.out.println();
-       commandHistory.add("echo");
+        for (String arg : args) {
+            System.out.print(arg + " ");
+        }
+        System.out.println();
+        commandHistory.add("echo");
     }
+
 
     public void pwdCommand(String[] args) {
         System.out.println(currentDirectory.toAbsolutePath());
         commandHistory.add("pwd");
 
     }
+
 
     public void changeDirectoryCommand(String[] args) {
         if (args.length == 0) {
@@ -170,7 +174,7 @@ public class Terminal {
 
                 if (Files.exists(newDirectory) && Files.isDirectory(newDirectory)) {
                     currentDirectory = newDirectory.toAbsolutePath();
-                    commandHistory.add("cd "+ newDirectoryPath);
+                    commandHistory.add("cd " + newDirectoryPath);
                 } else {
                     System.out.println("Directory does not exist: " + newDirectory);
                 }
@@ -179,6 +183,7 @@ public class Terminal {
             System.out.println("Usage: cd [<directory>|..]");
         }
     }
+
 
     public void listDirectoryCommand() {
         try {
@@ -191,6 +196,7 @@ public class Terminal {
             System.err.println("Error listing directory: " + e.getMessage());
         }
     }
+
 
     public void listDirectoryReverse() {
         try {
@@ -207,122 +213,101 @@ public class Terminal {
         }
     }
 
+
     public void makeDirectory(String[] args) {
         for (String arg : args) {
-                Path newPath;
-                if (Paths.get(arg).isAbsolute()) {
-                    newPath = Paths.get(arg);
-                } else {
-                    newPath = currentDirectory.resolve(arg);
-                }
-
-                try {
-                    Files.createDirectories(newPath);
-                    commandHistory.add("mkdir");
-                } catch (IOException e) {
-                    System.err.println("Error making directory: " + e.getMessage());
-                }
-            }
-    }
-
-    public void removeDirectory(String[] args) {
-        String path = currentDirectory.toAbsolutePath().toString();
-        if (args[0].equals("*")) {
-            File CD = new File(path);
-            removeEmptyDirectories(CD);
-            commandHistory.add("rmdir *");
-        }
-        else {
-            for (int i = 0; i < args.length; i++) {
-                String directoryPath = args[i];
-                File directory = new File(directoryPath);
-                if (!directory.exists()) {
-                    System.err.println("Directory does not exist: " + directoryPath);// Return an error code
-                }
-
-                if (!directory.isDirectory()) {
-                    System.err.println("Not a directory: " + directoryPath);// Return an error code
-                }
-
-                if (isDirectoryEmpty(directory)) {
-                    if (!directory.delete()) {
-                        System.err.println("Failed to remove directory: " + directoryPath);
-
-                    }
-                } else {
-                    System.err.println("Failed to remove: Directory not empty");
-
-                }
-            }
-
-        }
-    }
-    public static void removeEmptyDirectories(File directory) {
-        if (directory.isDirectory()) {
-            File[] files = directory.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    removeEmptyDirectories(file);
-                }
-            }
-            if (directory.list().length == 0) {
-                directory.delete();
-            }
-        }
-    }
-
-    private static boolean isDirectoryEmpty(File directory) {
-        if (directory.isDirectory()) {
-            String[] files = directory.list();
-            return files == null || files.length == 0;
-        }
-        return false;
-    }
-
-    public void createFile(String[] args) {
-        for (String arg : args) {
-                Path newPath;
-                if (Paths.get(arg).isAbsolute()) {
-                    newPath = Paths.get(arg);
-                } else {
-                    newPath = currentDirectory.resolve(arg);
-                }
-
-                try {
-                    Files.createFile(newPath);
-                    commandHistory.add("touch");
-                } catch (IOException e) {
-                    System.err.println("Error making file: " + e.getMessage());
-                }
-        }
-    }
-    public void copyFile(String[] args) {
-        Path firstPath;
-            Path secondPath;
-            if (Paths.get(args[1]).isAbsolute()) {
-                secondPath = Paths.get(args[1]);
+            Path newPath;
+            if (Paths.get(arg).isAbsolute()) {
+                newPath = Paths.get(arg);
             } else {
-                secondPath = currentDirectory.resolve(args[1]);
-            }
-            if (Paths.get(args[0]).isAbsolute()) {
-                firstPath = Paths.get(args[0]);
-            } else {
-                firstPath = currentDirectory.resolve(args[0]);
+                newPath = currentDirectory.resolve(arg);
             }
 
             try {
-                Files.copy(firstPath, secondPath);
-                commandHistory.add("cp");
+                Files.createDirectories(newPath);
+                commandHistory.add("mkdir");
             } catch (IOException e) {
-                System.err.println("Error copying file: " + e.getMessage());
+                System.err.println("Error making directory: " + e.getMessage());
+            }
         }
     }
+
+
+    public void removeDirectory(String[] args) {
+        for (String directoryPath : args) {
+            Path directory = currentDirectory.resolve(directoryPath);
+
+            if (!Files.exists(directory)) {
+                System.err.println("Directory does not exist: " + directory);
+                continue;
+            }
+
+            if (!Files.isDirectory(directory)) {
+                System.err.println("Not a directory: " + directory);
+                continue;
+            }
+
+            try {
+                Files.walk(directory)
+                        .sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+                System.out.println("Directory deleted: " + directory);
+                commandHistory.add("rmdir");
+            } catch (IOException e) {
+                System.err.println("Failed to remove directory: " + e.getMessage());
+            }
+        }
+    }
+
+
+    public void createFile(String[] args) {
+        for (String arg : args) {
+            Path newPath;
+            if (Paths.get(arg).isAbsolute()) {
+                newPath = Paths.get(arg);
+            } else {
+                newPath = currentDirectory.resolve(arg);
+            }
+
+            try {
+                Files.createFile(newPath);
+                commandHistory.add("touch");
+            } catch (IOException e) {
+                System.err.println("Error making file: " + e.getMessage());
+            }
+        }
+    }
+
+
+    public void copyFile(String[] args) {
+        Path firstPath;
+        Path secondPath;
+        if (Paths.get(args[1]).isAbsolute()) {
+            secondPath = Paths.get(args[1]);
+        } else {
+            secondPath = currentDirectory.resolve(args[1]);
+        }
+        if (Paths.get(args[0]).isAbsolute()) {
+            firstPath = Paths.get(args[0]);
+        } else {
+            firstPath = currentDirectory.resolve(args[0]);
+        }
+
+        try {
+            Files.copy(firstPath, secondPath);
+            commandHistory.add("cp");
+        } catch (IOException e) {
+            System.err.println("Error copying file: " + e.getMessage());
+        }
+    }
+
 
     public void cpDirectory(String[] args) {
         if (args.length != 2) {
             System.err.println("Usage: cp -r <source_directory> <destination_directory>");
             System.exit(1);
-        }else{
+        } else {
             Path firstPath = Paths.get(args[0]);
             Path secondPath = Paths.get(args[1]);
 
@@ -336,6 +321,8 @@ public class Terminal {
         }
 
     }
+
+
     public void copyDirectory(Path source, Path destination) throws IOException {
         Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
             @Override
@@ -352,6 +339,8 @@ public class Terminal {
             }
         });
     }
+
+
     public void removeFile(String[] args) {
         if (args.length == 1) {
             try {
@@ -370,6 +359,8 @@ public class Terminal {
             System.out.println("Invalid arguments for rm.");
         }
     }
+
+
     public void wordCount(String[] args) {
         if (args.length == 1) {
             try {
